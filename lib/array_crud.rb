1241880c7aae6ad0bd564db40afbcb8720519284
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-02 03:17:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-  fruits = ["apple", "banana", "strawberry", "pineapple"]
+  array = ["apple", "banana", "strawberry", "pineapple"]
 end
 
 def add_element_to_end_of_array(array, element)
-  fruits.push("cherry")
+  array.push("cherry")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift("melon")
 end
 
 def remove_element_from_end_of_array(array)
